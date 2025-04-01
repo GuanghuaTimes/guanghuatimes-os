9a1ae347f75e704ce1fe430012530e1038af61f4
光华时代 <pga@ghpga.com>
2025-04-01 13:57:20 +0800

    Update about.ts

--- config/about.ts
+++ config/about.ts
@@ -5,7 +5,7 @@ export const aboutConfig = {
   description:
     "老市村, located in 海头镇 of 儋州市, transitioned from its traditional salt farming to become a beacon of ecological sustainability and community renewal. The village faced ecological and social decline due to the shift from salt fields to shrimp ponds in the 1990s, leading to environmental damage and an aging population. In response, the 海南省蓝丝带海洋环境保护协会 initiated a restoration project, rehabilitating wetlands, planting mangroves, and promoting community involvement in conservation. Efforts expanded to cultural and economic revitalization, including the revival of ancient salt farming integrated with eco-tourism. These initiatives have earned international recognition, showcasing a successful model of combining ecological conservation with economic and cultural development for rural vitality.",
   descriptionCn:
-    "光华时代（海南）生物科技有限公司，是一家专注于高品质聚谷氨酸（γ-PGA）应用研发，集农业生产原料进出口贸易、其它农业生产资料研发生产及配套应用技术开发、咨询、服务、推广的农业应用型服务公司。\\ 集团总公司为武汉光华时代生物科技有限公司，总公司成立于2013年，是一家集科研、生产、销售、服务于一体的综合型高新技术企业。\\ 公司以华中农业大学、湖北大学、武汉轻工业大学为技术依托（以陈守文教授为核心的技术团队100余人，拥有发明专利30多项，拥有全套的聚谷氨酸、褐藻寡糖产品柔性生产线），专业从事聚谷氨酸（γ-PGA）、褐藻寡糖（AOS）及功能性微生物菌的研发、生产、技术应用及渠道销售。",
+    "光华时代（海南）生物科技有限公司，是一家专注于高品质聚谷氨酸（γ-PGA）应用研发，集农业生产原料进出口贸易、其它农业生产资料研发生产及配套应用技术开发、咨询、服务、推广的农业应用型服务公司。<br><br>集团总公司为武汉光华时代生物科技有限公司，总公司成立于2013年，是一家集科研、生产、销售、服务于一体的综合型高新技术企业。<br><br>公司以华中农业大学、湖北大学、武汉轻工业大学为技术依托（以陈守文教授为核心的技术团队100余人，拥有发明专利30多项，拥有全套的聚谷氨酸、褐藻寡糖产品柔性生产线），专业从事聚谷氨酸（γ-PGA）、褐藻寡糖（AOS）及功能性微生物菌的研发、生产、技术应用及渠道销售。",
   navs: [
     {
       imgSrc: "/hero_temple.jpeg",
