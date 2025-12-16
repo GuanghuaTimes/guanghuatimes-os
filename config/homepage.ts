87f2d614792336a7c4126321680dfa93de1333d6
光华时代 <pga@ghpga.com>
2025-12-15 23:06:58 -0800

    Update homepage.ts

--- config/homepage.ts
+++ config/homepage.ts
@@ -1,25 +1,23 @@
 export const homepageConfig = {
-  carouselItems: [
-  // 第一个轮播项
-  {
-    imgSrc: '/product-images/（AGRARIUS）小粒低聚型20kg袋装370X750X80mm（效果图）20250324.jpg',
-    title: 'AGRARIUS 小粒低聚型有机水溶肥料',
-    titleCn: 'Welcome to 光华时代',
-    description: "Innovation-driven, quality creates the future; Technology empowers, service creates value!",
-    descriptionCn: '营养全面全水快溶, 调理土壤, 增肥力.',
-    href: '/about',
-  }, // <--- 这里是数组元素分隔，必须有逗号
-  // 第二个轮播项
-  {
-    imgSrc: '/product-images/（AGRARIUS）生物蛋白肽20kg（效果图）20250324.png',
-    title: 'AGRARIUS 生物蛋白肽有机水溶肥料',
-    titleCn: 'Welcome to 光华时代',
-    description: "Innovation-driven, quality creates the future; Technology empowers, service creates value!",
-    descriptionCn: '富含聚谷氨酸、有机质, 促进根系生长, 增强土壤肥力.',
-    href: '/nature-school',
-  } 
-],
-  navs: [,
+ carouselItems: [
+    {
+      imgSrc: '/product-images/（AGRARIUS）小粒低聚型20kg袋装370X750X80mm（效果图）20250324.jpg',
+      title: 'AGRARIUS 小粒低聚型有机水溶肥料',
+      titleCn: 'Welcome to 光华时代',
+      description: "Innovation-driven, quality creates the future; Technology empowers, service creates value!",
+      descriptionCn: '营养全面全水快溶，调理土壤，增肥力。',
+      href: '/about'
+    },
+    {
+      imgSrc: '/product-images/（AGRARIUS）生物蛋白肽20kg（效果图）20250324.png',
+      title: 'AGRARIUS 生物蛋白肽有机水溶肥料',
+      titleCn: 'Welcome to 光华时代',
+      description: "Innovation-driven, quality creates the future; Technology empowers, service creates value!",
+      descriptionCn: '富含聚谷氨酸、有机质，促进根系生长，增强土壤肥力。',
+      href: '/nature-school'
+    }
+  ],
+  navs: [
     {
       imgSrc: "/8871718163756_.pic.jpg",
       title: "About Guanghua Times",
